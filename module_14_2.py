774b49edfd3884399eff9aec6fdcf68099096168
astakhova-kv <prisakarkv@gmail.com>
2024-12-09 10:37:16 +0700

    Add files via upload

--- module_14_2.py
+++ module_14_2.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-connection = sqlite3.connect('not_telegram.db')
+connection = sqlite3.connect('not_telegram_2.db')
 cursor = connection.cursor()
 
 cursor.execute('''
@@ -21,12 +21,13 @@ balance INTEGER NOT NULL
 
 # for i in range(1, 11):
 #     cursor.execute('DELETE FROM Users WHERE id % 3 = ?', (1,))
-# cursor.execute('DELETE FROM Users WHERE id = ?', (6,))
 
 # cursor.execute('SELECT username, email, age, balance FROM Users WHERE age != ?', (60,))
 # users = cursor.fetchall()
 # for user in users:
 #     print(f'Имя: {user[0]} | Почта: {user[1]} | Возраст: {user[2]} | Баланс: {user[3]}')
+
+cursor.execute('DELETE FROM Users WHERE id = ?', (6,))
 
 cursor.execute('SELECT COUNT(*) FROM Users')
 total1 = cursor.fetchone()[0]
